7032a5651165fcf11eda96fb1f3c69dd345fa276
wangqf <15376960583@189.cn>
2021-12-15 17:00:25 +0800

    init

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ['plugin:vue/essential', 'airbnb-base', 'plugin:prettier/recommended', 'prettier'],
+  parserOptions: {
+    ecmaVersion: 13,
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module',
+  },
+  plugins: ['vue', '@typescript-eslint'],
+  rules: {
+    // 允许template存在多个根节点
+    'vue/no-multiple-template-root': 'off',
+  },
+  globals: {
+    defineProps: 'readonly',
+    defineEmits: 'readonly',
+    defineExpose: 'readonly',
+    withDefaults: 'readonly',
+  },
+}
---
+++ vite.config.ts
@@ -0,0 +1,42 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
+import VitePluginHtmlEnv from 'vite-plugin-html-env'
+
+export default defineConfig({
+  // 插件
+  plugins: [vue(), VitePluginHtmlEnv()],
+  // 解析
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src'),
+      '~': resolve(__dirname, './'),
+    },
+    extensions: ['.js', '.ts', '.jsx', '.tsx', '.json', '.vue', '.mjs'],
+  },
+  // css处理
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: '@import "./src/assets/style/variable.scss";',
+      },
+    },
+  },
+  // 根路径
+  base: './',
+  // cli服务
+  server: {
+    host: true,
+    port: 8080,
+    open: true,
+    cors: true,
+    // proxy: {
+    //   '/api': {
+    //     target: '',
+    //     changeOrigin: true,
+    //     secure: false,
+    //     rewrite: (path) => path.replace('/api/', '/'),
+    //   },
+    // },
+  },
+})
